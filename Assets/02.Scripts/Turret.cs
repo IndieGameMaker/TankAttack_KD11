f53db28c5567d39fd0f62ad6b51f93942e4fb840
Lee JaeHyun <myleje@gmail.com>
2023-03-16 13:21:49 +0900

    .

---
+++ Assets/02.Scripts/Turret.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    private Transform tr;
+    private RaycastHit hit;
+
+    [SerializeField] private float turnSpeed = 10.0f;
+
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        // 메인 카메라에서 레이케스트를 발사
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * Mathf.Infinity, Color.green);
+    }
+}
