e2490b3bf82757a5551d0096292ebf0ce736058c
Lee JaeHyun <myleje@gmail.com>
2023-03-17 11:18:16 +0900

    .

--- Assets/02.Scripts/Cannon.cs
+++ Assets/02.Scripts/Cannon.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Cannon : MonoBehaviour
 {
     [SerializeField] private float speed = 2000.0f;
+    [SerializeField] private GameObject expEffect;
 
     void Start()
     {
@@ -14,7 +16,13 @@ public class Cannon : MonoBehaviour
 
     void OnCollisionEnter()
     {
+        // 무작위 각도 생성
+        Quaternion rot = Quaternion.Euler(Vector3.up * Random.Range(0, 360));
+        var obj = Instantiate(expEffect, transform.position, rot);
+
         Destroy(this.gameObject);
+
+        Destroy(obj, 6.0f);
     }
 
 }
